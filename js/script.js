41707bc5bea6179f352ba20e0bc2e6814e53aefb
vijaymoorthy13 <85095015+vijaymoorthy13@users.noreply.github.com>
2022-03-31 22:23:03 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "100%";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.body.style.backgroundColor = "rgba(0,0,0,0)";
+}
